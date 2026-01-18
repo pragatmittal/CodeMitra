5fe930eabf35e827d6de9159641ae7bec4ac0026
Pragat Mittal <143354114+pragatmittal@users.noreply.github.com>
2026-01-18 19:14:03 +0530

    Add types for socket user and authenticated socket

---
+++ backend/src/socket/types.ts
@@ -0,0 +1,16 @@
+import { Server as SocketIOServer } from 'socket.io';
+import { Socket } from 'socket.io';
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar?: string | null;
+}
+
+export interface AuthenticatedSocket extends Socket {
+  userId?: string;
+  user?: User;
+}
+
+export type Server = SocketIOServer;
